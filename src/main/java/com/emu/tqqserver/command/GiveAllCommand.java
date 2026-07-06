d07414cdfe3bad53f8a479be6d876d90d6d16573
Furiri <raidenshogun558@icloud.com>
2026-07-06 18:26:35 +0700

    feat: add GiveAllCommand to grant cards and items to users

--- src/main/java/com/emu/tqqserver/command/GiveAllCommand.java
+++ src/main/java/com/emu/tqqserver/command/GiveAllCommand.java
@@ -24,7 +24,7 @@ public class GiveAllCommand implements ICommand {
 
     @Override
     public String getDescription() {
-        return "Gives all cards and items to the specified user. Usage: /give_all <userId>";
+        return "Gives all cards and items to the specified user. Usage: /give_all <userId> [type: cards|items|backgrounds|clothes|all]";
     }
 
     @Override
@@ -42,68 +42,80 @@ public class GiveAllCommand implements ICommand {
             return;
         }
 
+        String type = "all";
+        if (args.length >= 3) {
+            type = args[2].toLowerCase();
+        }
+
         String masterDir = System.getProperty("gotopazu.resource.list.dir", "gotopazu");
         File cardFile = new File(masterDir, "master/card.json");
         File itemFile = new File(masterDir, "master/item.json");
 
         try {
             // Give all cards
-            if (cardFile.exists()) {
-                JsonNode cardArray = mapper.readTree(cardFile);
-                List<Integer> cardIds = new ArrayList<>();
-                if (cardArray.isArray()) {
-                    for (JsonNode cardNode : cardArray) {
-                        cardIds.add(cardNode.get("id").asInt());
+            if (type.equals("all") || type.equals("cards")) {
+                if (cardFile.exists()) {
+                    JsonNode cardArray = mapper.readTree(cardFile);
+                    List<Integer> cardIds = new ArrayList<>();
+                    if (cardArray.isArray()) {
+                        for (JsonNode cardNode : cardArray) {
+                            cardIds.add(cardNode.get("id").asInt());
+                        }
                     }
+                    
+                    int[] cards = cardIds.stream().mapToInt(i -> i).toArray();
+                    userDao.ensureDefaultCards(targetUserId, cards);
+                    log.info("Successfully gave {} cards to user {}", cards.length, targetUserId);
+                } else {
+                    log.warn("card.json not found in {}", masterDir);
                 }
-                
-                int[] cards = cardIds.stream().mapToInt(i -> i).toArray();
-                userDao.ensureDefaultCards(targetUserId, cards);
-                log.info("Successfully gave {} cards to user {}", cards.length, targetUserId);
-            } else {
-                log.warn("card.json not found in {}", masterDir);
             }
 
             // Give all items
-            if (itemFile.exists()) {
-                JsonNode itemArray = mapper.readTree(itemFile);
-                if (itemArray.isArray()) {
-                    for (JsonNode itemNode : itemArray) {
-                        int itemId = itemNode.get("id").asInt();
-                        userService.addItem(targetUserId, itemId, 999);
+            if (type.equals("all") || type.equals("items")) {
+                if (itemFile.exists()) {
+                    JsonNode itemArray = mapper.readTree(itemFile);
+                    if (itemArray.isArray()) {
+                        for (JsonNode itemNode : itemArray) {
+                            int itemId = itemNode.get("id").asInt();
+                            userService.addItem(targetUserId, itemId, 999);
+                        }
                     }
+                    log.info("Successfully gave all items to user {}", targetUserId);
+                } else {
+                    log.warn("item.json not found in {}", masterDir);
                 }
-                log.info("Successfully gave all items to user {}", targetUserId);
-            } else {
-                log.warn("item.json not found in {}", masterDir);
             }
 
             // Give all backgrounds
-            File bgFile = new File(masterDir, "master/home_background.json");
-            if (bgFile.exists()) {
-                JsonNode bgArray = mapper.readTree(bgFile);
-                if (bgArray.isArray()) {
-                    for (JsonNode bgNode : bgArray) {
-                        int bgId = bgNode.get("id").asInt();
-                        userService.addHomeBackground(targetUserId, bgId);
+            if (type.equals("all") || type.equals("backgrounds")) {
+                File bgFile = new File(masterDir, "master/home_background.json");
+                if (bgFile.exists()) {
+                    JsonNode bgArray = mapper.readTree(bgFile);
+                    if (bgArray.isArray()) {
+                        for (JsonNode bgNode : bgArray) {
+                            int bgId = bgNode.get("id").asInt();
+                            userService.addHomeBackground(targetUserId, bgId);
+                        }
                     }
+                    log.info("Successfully gave all home backgrounds to user {}", targetUserId);
                 }
-                log.info("Successfully gave all home backgrounds to user {}", targetUserId);
             }
 
             // Give all clothes
-            File clothesFile = new File(masterDir, "master/home_member_clothes.json");
-            if (clothesFile.exists()) {
-                JsonNode clothesArray = mapper.readTree(clothesFile);
-                if (clothesArray.isArray()) {
-                    for (JsonNode clothesNode : clothesArray) {
-                        int clothesId = clothesNode.get("id").asInt();
-                        userService.addClothes(targetUserId, clothesId);
+            if (type.equals("all") || type.equals("clothes")) {
+                File clothesFile = new File(masterDir, "master/home_member_clothes.json");
+                if (clothesFile.exists()) {
+                    JsonNode clothesArray = mapper.readTree(clothesFile);
+                    if (clothesArray.isArray()) {
+                        for (JsonNode clothesNode : clothesArray) {
+                            int clothesId = clothesNode.get("id").asInt();
+                            userService.addClothes(targetUserId, clothesId);
+                        }
                     }
+                    log.info("Successfully gave all clothes to user {}", targetUserId);
                 }
-                log.info("Successfully gave all clothes to user {}", targetUserId);
             }
-
         } catch (Exception e) {
             log.error("Failed to execute give_all command", e);
         }
